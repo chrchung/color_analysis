8ddd3720bffca73ccc9faff64d0282f2ff45c64a
oreoreo <catherinechen1002@hotmail.com>
2017-06-29 22:02:04 -0400

    corrected script and updated base csv file with color ids

--- base.py
+++ base.py
@@ -3,20 +3,21 @@ import csv
 
 #change[primID] = correct base color
 change = {}
-with open('A:/color_analysis/base.csv', 'rb') as inp, open('A:/color_analysis/base_edit.csv', 'wb') as out:
+with open('A:/color_analysis/base_id.csv', 'rb') as inp, open('A:/color_analysis/base_edit.csv', 'wb') as out:
     basereader = csv.reader(inp, delimiter=' ', quotechar='|')
     writer = csv.writer(out)
     mark = False
     for row in csv.reader(inp):
         #null val, get base col from next row
-        if row[1] == 'NULL':
+        if row[2] == 'NULL':
             mark = True
-            id = row[2]
+            change[row[0]] = 'NULL'
             continue
         
         if mark:
-            change[id] = row[2]
-            mark = False
+            for item in change:
+                if change[item] == 'NULL':
+                    change[item] = row[0]
         #write non-duplicate rows to new csv
         writer.writerow(row)
         
